dcb47c6da005e57dff2c163317afe7ddae9f5d87
Clay Coleman <coleclayman@gmail.com>
2015-10-16 17:21:27 -0600

    fixed the bug introduced by last commit

--- project/urls.py
+++ project/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     # url(pattern, view)
     url(r'^state_list/$', 'main.views.state_list', name="state_list"),
     url(r'^$', 'main.views.home', name="home"),
-
+    url(r'^home/$', 'main.views.home', name="home2"),
     url(r'^state_detail/(?P<pk>\d+)/$', 'main.views.state_detail', name='state_detail'),
     url(r'^city_list/$', 'main.views.city_search', name='city_search'),
     url(r'^city_detail/(?P<pk>\d+)/$', 'main.views.city_detail', name='city_detail'),
